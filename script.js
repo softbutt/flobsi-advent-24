b225784b9d5d825b472620b1723903f8de48f5fd
florian.buttinger@paso-solutions.com <florian.buttinger@paso-solutions.com>
2024-12-01 22:30:23 +0100

    styling

--- script.js
+++ script.js
@@ -4,36 +4,8 @@ const todayDate = new Date();
 const cards = [];
 
 document.addEventListener('DOMContentLoaded', () => {
-    // const today = new Date().getDate();
-    // const month = new Date().getMonth() + 1;
-    // const year = new Date().getFullYear();
-    // const cards = document.querySelectorAll('.card');
-
-    // document.getElementById("date").textContent = "Heute ist der " + today + ". Dezember";
-
-    // if(month !== 12 || year !== 2024) {
-    //     document.getElementsByTagName("body")[0].innerHTML = "<h1>Nicht verfügbar!</h1>";
-    //     return;
-    // }
-
     initToday();
     initPresents();
-
-    // cards.forEach(card => {
-    //     const day = parseInt(card.getAttribute('data-day'), 10);
-    //     card.addEventListener('click', () => {
-    //         if (day <= today) {
-    //             if(card.classList.contains('flipped')) {
-    //                 card.classList.remove('flipped');
-    //             } else {
-    //                 card.classList.add('flipped');
-    //                 setTimeout(() => {
-    //                     card.classList.remove('flipped');
-    //                 }, 10000);
-    //             }
-    //         }
-    //     });
-    // });
 });
 
 const initToday = () => {
@@ -46,7 +18,7 @@ const initToday = () => {
     }
 
     const today = todayDate.getDate();
-    document.getElementById("date").textContent = "Heute ist der " + today + ". Dezember";
+    document.getElementById("date").textContent = "am " + today + ". Dezember";
 }
 
 const initPresents = () => {
@@ -98,7 +70,7 @@ const createCard = (master, container, present) => {
         }
     });
 
-    container.appendChild(clone);
+    container.prepend(clone);
 };
 
 const getCategoryEmojy = (category) => {
